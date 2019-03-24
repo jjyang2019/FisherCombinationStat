82a75095fe29ba639b90fc255ed501f78c92f1d8
jjyang2019 <48845579+jjyang2019@users.noreply.github.com>
2019-03-23 20:36:22 -0400

    use pcaPP::cor.fk to calculate Kendall's tau
    cor(x,y,method="kendall") is too slow. Use pcaPP::cor.fk to improve the speed.

--- R/FisherCombPval.R
+++ R/FisherCombPval.R
@@ -82,7 +82,12 @@ cal.cor.pearson <- function(x,y){
 ### internal functions
 
 cal.cor.kendall <- function(x,y){
-    tau <- cor(x,y, method="kendall")
+    if(is.element("pcaPP", installed.packages()[,1])){ 
+        tau <- pcaPP::cor.fk(x,y)
+    }else{
+        cat("\nPackage pcaPP is not installed, use stats:cor\n")
+        tau <- cor(x,y, method="kendall")
+    }
     ## use="pairwise.complete.obs")
     ## tau <- cor.fk(x)
     r <- sin(pi*tau/2)
